47489bdefe1709cfda36d4a8bf635d2377e857c5
qin <qin@192.168.1.16>
2019-07-04 10:37:07 +0800

    添加Student类

---
+++ HalloWorld/src/com/qjy/entity/Student.java
@@ -0,0 +1,47 @@
+package com.qjy.entity;
+
+import java.util.Date;
+
+public class Student {
+	
+	private String name;
+	
+	private Boolean sex;
+	
+	private String number;
+	
+	private Date birthday;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getSex() {
+		return sex;
+	}
+
+	public void setSex(Boolean sex) {
+		this.sex = sex;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+}
--- HalloWorld/src/com/qjy/text/Halloworld.java
+++ HalloWorld/src/com/qjy/text/Halloworld.java
@@ -5,5 +5,6 @@ public class Halloworld {
 	public static void main(String[] args) {
 		System.out.println("Halloworld!");
 		System.out.println("11111111111");
+		System.out.println("22222222222");
 	}
 }
